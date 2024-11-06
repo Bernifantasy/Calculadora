6cb36cbaa8013e24c04b0e6fd8241c950e549f8b
Bernifantasy <150950776+Bernifantasy@users.noreply.github.com>
2024-11-06 12:43:00 +0100

    pagina index actualitzada amb login

---
+++ calculadora_simple/script.js
@@ -0,0 +1,233 @@
+let operand1="0";
+let operand2="";
+let operador="";
+let errorMsg="";
+const Max_Llargada=5;
+
+    document.addEventListener("DOMContentLoaded", function () {
+    operand1 = "0";
+    document.getElementById("pantallaresultat").value = operand1;
+  });
+
+function Comprovar_Operand(operand)
+{
+    if(operand.length > Max_Llargada)
+    {
+        return [1, "Massa Llarg"];
+    }else
+    {
+        return [0,"Tot correcte"];
+    }
+
+}
+
+
+function Tractar_Un()
+{
+    operand1 += "1";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Dos()
+{
+    operand1 += "2";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Tres()
+{
+    operand1 += "3";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Quatre()
+{
+    operand1 += "4";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Cinc()
+{
+    operand1 += "5";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Sis()
+{
+    operand1 += "6";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Set()
+{
+    operand1 += "7";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Vuit()
+{
+    operand1 += "8";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Nou()
+{
+    operand1 += "9";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
+
+function Tractar_Zero()
+{
+    operand1 += "0";
+    let pantalla = document.getElementById("pantallaresultat");
+    let pantalla_info = document.getElementById("pantallainfo");
+
+    let [error, errorMsg]=Comprovar_Operand(operand1);
+
+    if(error===0)
+    {
+        pantalla.value = operand1;
+        pantalla_info.value = "";
+    }
+    else
+    {
+        operand1 = operand1.substring(0, operand1.length - 1);
+        console.log("error desfem operand1" + operand1);
+        pantalla_info.value = errorMsg;
+    }
+}
