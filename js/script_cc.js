0ddd7ce71f001395c39a29364ed2775dd3bcaff2
Bernifantasy <150950776+Bernifantasy@users.noreply.github.com>
2024-11-10 17:10:23 +0100

    Update script_cc.js

--- js/script_cc.js
+++ js/script_cc.js
@@ -1,93 +1,529 @@
-let operand1 = "0";
-        let operand2 = "";
-        let operador = "";
+let operand1="0";
+let operand2="";
+let operador="";
+let errorMsg="";
+const Max_Llargada=5;
+const Separador_Decimal=".";
+const pantalla=document.getElementById("pantallaresultat")
 
-        function mostrarPantalla() {
-            let pantalla = document.getElementById("pantallaresultat");
-            pantalla.value = operand1 + operador + operand2;
+function mostrarPantalla()
+{
+    document.getElementById("pantallaresultat").value=operand1 + operador + operand2;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    operand1 = "0";
+    document.getElementById("pantallaresultat").value = operand1;
+  });
+
+function Comprovar_Operand(operand)
+{
+    if(operand.length > Max_Llargada)
+    {
+        return [1, "Massa Llarg"];
+    }else
+    {
+        return [0,"Tot correcte"];
+    }
+
+}
+
+function Tractar_Un()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";  
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "1";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "1";
+            } 
+    }
+    
+    let pantalla = document.getElementById("pantallaresultat");
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
         }
+    }
+}
 
-        function Clear() {
-            operand1 = "0";
-            operand2 = "";
-            operador = "";
+
+function Tractar_Dos()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";  
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "2";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "2";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+function Tractar_Tres()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = ""; 
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "3";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "3";
+            } 
+    }
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+        } 
+        else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+function Tractar_Quatre()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";  
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "4";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "4";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+function Tractar_Cinc()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "5";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "5";
+            } 
+    }
+    
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+function Tractar_Sis()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = ""; 
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "6";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "6";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+function Tractar_Set()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";  
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "7";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "7";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+function Tractar_Vuit()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "8";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "8";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+function Tractar_Nou()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = ""; 
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "9";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "9";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+function Tractar_Zero()
+{
+    if (operand1 === "0" && operador === "") {
+        operand1 = "";
+    }
+    if(operador==="")
+    {
+        if(operand1.length<Max_Llargada)
+        {
+            operand1 += "0";
+        }        
+    }else{
+        if(operand2.length<Max_Llargada)
+            {
+                operand2 += "0";
+            } 
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+function Tractar_Coma()
+{
+    if(operador==="")
+    {
+        if(!operand1.includes(Separador_Decimal))
+        {
+            operand1+=Separador_Decimal;
+        }
+    }else{
+        if(!operand2.includes(Separador_Decimal))
+        {
+            operand2+=Separador_Decimal;
+        }
+    }
+
+    let [error1, errorMsg1]=Comprovar_Operand(operand1);
+    let [error2, errorMsg2]=Comprovar_Operand(operand2);
+
+    if (error1 === 0 && error2 === 0) {
+        pantalla.value = operand1 + operador + operand2;
+    } else {
+        if (error1 !== 0) {
+            operand1 = operand1.substring(0, operand1.length - 1);
+        } else if (error2 !== 0) {
+            operand2 = operand2.substring(0, operand2.length - 1);
+        }
+    }
+}
+
+
+
+function Clear()
+{
+    operand1="";
+    operand2="";
+    operador="";
+    document.getElementById("pantallaresultat").value="0";
+}
+
+function Sumar()
+{
+    if(operand1 !== "" && operador ==="" && operand2 === "")
+    {
+        operador="+";
+        mostrarPantalla();
+    }else if(operand1 !=="" && operador !== "" && operand2 !== "")
+    {
+        Igual();
+        operador="+";
+    }
+}
+
+function Restar()
+{
+    if(operand1 !== "" && operador ==="" && operand2 === "")
+        {
+            operador="-";
             mostrarPantalla();
+        }else if(operand1 !=="" && operador !== "" && operand2 !== "")
+        {
+            Igual();
+            operador="-";
         }
 
-        function Esborrar_un() {
-            if (operador === "") {
-                operand1 = operand1.slice(0, -1) || "0";
-            } else {
-                operand2 = operand2.slice(0, -1);
-            }
+}
+
+function Multiplicar()
+{
+    if(operand1 !== "" && operador ==="" && operand2 === "")
+        {
+            operador="*";
             mostrarPantalla();
+        }else if(operand1 !=="" && operador !== "" && operand2 !== "")
+        {
+            Igual();
+            operador="*";
         }
 
-        function Sumar() { operador = "+"; mostrarPantalla(); }
-        function Restar() { operador = "-"; mostrarPantalla(); }
-        function Multiplicar() { operador = "*"; mostrarPantalla(); }
-        function Dividir() { operador = "/"; mostrarPantalla(); }
+}
 
-        function Igual() {
-            let resultado;
-            const num1 = parseFloat(operand1);
-            const num2 = parseFloat(operand2);
-
-            switch (operador) {
-                case "+": resultado = num1 + num2; break;
-                case "-": resultado = num1 - num2; break;
-                case "*": resultado = num1 * num2; break;
-                case "/": resultado = num2 !== 0 ? num1 / num2 : "Error"; break;
-            }
-            operand1 = resultado.toString();
-            operador = "";
-            operand2 = "";
+function Dividir()
+{
+    if(operand1 !== "" && operador ==="" && operand2 === "")
+        {
+            operador="/";
             mostrarPantalla();
+        }else if(operand1 !=="" && operador !== "" && operand2 !== "") 
+        {
+            Igual();
+            operador="/";
         }
 
-        function Tractar_Numero(num) {
-            if (operador === "") {
-                operand1 = operand1 === "0" ? num.toString() : operand1 + num;
-            } else {
-                operand2 += num;
-            }
-            mostrarPantalla();
-        }
+}
 
-        function Tractar_Set() { Tractar_Numero(7); }
-        function Tractar_Vuit() { Tractar_Numero(8); }
-        function Tractar_Nou() { Tractar_Numero(9); }
-        function Tractar_Quatre() { Tractar_Numero(4); }
-        function Tractar_Cinc() { Tractar_Numero(5); }
-        function Tractar_Sis() { Tractar_Numero(6); }
-        function Tractar_Un() { Tractar_Numero(1); }
-        function Tractar_Dos() { Tractar_Numero(2); }
-        function Tractar_Tres() { Tractar_Numero(3); }
-        function Tractar_Zero() { Tractar_Numero(0); }
-        function Tractar_Coma() { if (!operand1.includes(".")) operand1 += "."; mostrarPantalla(); }
 
-        // Funciones científicas
-        function Calcular_Seno() {
-            operand1 = Math.sin(parseFloat(operand1)).toString();
-            mostrarPantalla();
-        }
+function Igual()
+{
+    if(operand1 !=="" && operador!=="" && operand2!=="")
+    {
+        let resultat;
+        const num1= parseFloat(operand1);
+        const num2=parseFloat(operand2);
 
-        function Calcular_Coseno() {
-            operand1 = Math.cos(parseFloat(operand1)).toString();
-            mostrarPantalla();
+        switch(operador)
+        {
+            case"+":
+                resultat= num1+num2;
+                break;
+            case"-":
+                resultat=num1-num2;
+                break;
+            case"*":
+                resultat=num1*num2;
+                break;
+            case"/":
+                if(num2!==0)    
+                {
+                    resultat=num1/num2;
+                }
+                else{
+                    resultat="Error"
+                }
+                break;
         }
+        operand1=resultat.toString();
+        document.getElementById("pantallaresultat").value=operand1;
+        operador="";
+        operand1="";
+        operand2="";
 
-        function Calcular_Tangente() {
-            operand1 = Math.tan(parseFloat(operand1)).toString();
-            mostrarPantalla();
-        }
+    }
+}
 
-        function Calcular_Log() {
-            operand1 = Math.log10(parseFloat(operand1)).toString();
-            mostrarPantalla();
-        }
+function Esborrar_un()
+{
 
-        function Calcular_Raiz() {
-            operand1 = Math.sqrt(parseFloat(operand1)).toString();
-            mostrarPantalla();
-        }
+}
+
+
+function Calcular_Seno() {
+    operand1 = Math.sin(parseFloat(operand1)).toString();
+    mostrarPantalla();
+}
+
+function Calcular_Coseno() {
+    operand1 = Math.cos(parseFloat(operand1)).toString();
+    mostrarPantalla();
+}
+
+function Calcular_Tangente() {
+    operand1 = Math.tan(parseFloat(operand1)).toString();
+    mostrarPantalla();
+}
+
+function Calcular_Log() {
+    operand1 = Math.log10(parseFloat(operand1)).toString();
+    mostrarPantalla();
+}
+
+function Calcular_Raiz() {
+    operand1 = Math.sqrt(parseFloat(operand1)).toString();
+    mostrarPantalla();
+}
